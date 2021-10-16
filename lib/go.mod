2d05d02ee912291d259b052561d16fda9eff6691
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2021-10-16 09:55:24 -0700

    Add lib/go.mod

---
+++ lib/go.mod
@@ -0,0 +1,3 @@
+module github.com/fishy/test-go-mod-retract/lib
+
+go 1.17
---
+++ lib/lib.go
@@ -0,0 +1,5 @@
+// Package lib provides a dummy library for test purpose
+package lib
+
+// Foo is a dummy public constant.
+const Foo = 1234
